fe54b31ef4c9fb72e95059622165937f628f9881
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-13 16:32:19 +0600

    service section updated

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -18,55 +18,55 @@ const Services = () => {
       <div className="px-3 md:px-28 pb-5 pt-2 h-screen service-container">
         <div className="mt-10 md:mt-20 grid grid-cols-1 md:grid-cols-4 gap-10 items-center text-center">
           <Link to="/businessmanage">
-            <div className="py-9 px-9 bg-base-100 mx-auto rounded-xl shadow-md">
+            <div className="py-9 px-9 bg-base-100 hover:bg-secondary duration-500  mx-auto rounded-xl shadow-md text-secondary hover:text-base-100">
               <img className="w-16 mx-auto mb-2" src={service1} alt="" />
-              <p className="text-secondary">
+              <p>
                 Business Management <br /> Software
               </p>
             </div>
           </Link>
           <Link to="/digitalmarketing">
-            <div className="px-14 py-9 bg-base-100 mx-auto rounded-xl shadow-md">
+            <div className="px-14 py-9 bg-base-100 mx-auto rounded-xl shadow-md text-secondary hover:bg-secondary hover:text-base-100 duration-500">
               <img className="w-16 mx-auto mb-2" src={service2} alt="" />
-              <p className="text-secondary">
+              <p>
                 Digital Marketing <br /> Solutions
               </p>
             </div>
           </Link>
           <Link to="/mobileapp">
-            <div className="px-7 py-10 bg-base-100 rounded-xl">
+            <div className="px-7 py-10 bg-base-100 rounded-xl text-secondary hover:bg-secondary hover:text-base-100 duration-500">
               <img className="w-16 mx-auto mb-2" src={service3} alt="" />
-              <p className="text-secondary">Mobile app & Web Development</p>
+              <p>Mobile app & Web Development</p>
             </div>
           </Link>
           <Link to="/ecommerce">
-            <div className="py-12 bg-base-100 rounded-lg">
+            <div className="py-12 bg-base-100 rounded-lg text-secondary hover:bg-secondary hover:text-base-100 duration-500">
               <img className="w-16 mx-auto mb-2" src={service4} alt="" />
-              <p className="text-secondary">E-Commerce Solution</p>
+              <p>E-Commerce Solution</p>
             </div>
           </Link>
           <Link to="/affiliate">
-            <div className="py-10 px-7 bg-base-100 rounded-lg">
+            <div className="py-10 px-7 bg-base-100 rounded-lg text-secondary hover:bg-secondary hover:text-base-100 duration-500">
               <img className="w-24 mx-auto mb-2" src={service5} alt="" />
-              <p className="text-secondary">Affiliate Marketing Solutions</p>
+              <p>Affiliate Marketing Solutions</p>
             </div>
           </Link>
           <Link to="/adnetwork">
-            <div className="py-12 px-8 rounded-lg bg-base-100">
+            <div className="py-12 px-8 rounded-lg bg-base-100 text-secondary hover:bg-secondary hover:text-base-100 duration-500">
               <img className="w-16 mx-auto mb-2" src={service6} alt="" />
-              <p className="text-secondary">Ad Network Solutions</p>
+              <p>Ad Network Solutions</p>
             </div>
           </Link>
           <Link to="/personalbranding">
-            <div className="py-12 px-8 rounded-lg bg-base-100">
+            <div className="py-12 px-8 rounded-lg bg-base-100 text-secondary hover:bg-secondary hover:text-base-100 duration-500">
               <img className="w-16 mx-auto mb-2" src={service7} alt="" />
-              <p className="text-secondary">Personal Branding</p>
+              <p>Personal Branding</p>
             </div>
           </Link>
           <Link to="/bulksms">
-            <div className="py-12 px-8 rounded-lg bg-base-100">
+            <div className="py-12 px-8 rounded-lg bg-base-100 text-secondary hover:bg-secondary hover:text-base-100 duration-500">
               <img className="w-16 mx-auto mb-2" src={service8} alt="" />
-              <p className="text-secondary">Bulk SMS Services</p>
+              <p>Bulk SMS Services</p>
             </div>
           </Link>
         </div>
