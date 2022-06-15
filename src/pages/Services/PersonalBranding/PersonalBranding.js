74b89190baceb64d4cd0e6094fad7fc384be752e
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-15 13:05:53 +0600

    personal branding done

--- src/pages/Services/PersonalBranding/PersonalBranding.js
+++ src/pages/Services/PersonalBranding/PersonalBranding.js
@@ -10,15 +10,17 @@ import Typewriter from "typewriter-effect";
 
 const PersonalBranding = () => {
   return (
-    <div className="branding-container">
-      <SubNavbar></SubNavbar>
-      <div className="bg-base-200 h--full">
+    <div className="">
+      <div className="hidden md:block md:sticky  md:top-20 md:z-50">
+        <SubNavbar></SubNavbar>
+      </div>
+      <div className="bg-base-200 h-full pb-10">
         <div className="px-5 md:px-20 pt-10 h-screen">
-          <div class="flex">
-            <div>
+          <div class="md:flex">
+            <div className="order-2 md:order-1">
               <img className="h-full rounded-lg shadow-2xl" src={photo8} />
             </div>
-            <div className="px-36 mt-20">
+            <div className="md:px-36 mt-20 order-1 md:order-2">
               <h1 class="text-4xl text-primary text-center mb-4">
                 PERSONAL BRANDING
               </h1>
@@ -36,13 +38,13 @@ const PersonalBranding = () => {
             </div>
           </div>
         </div>
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center mt-48">
           <div>
             <img className="w-56 branding-img" src={photo1} alt="" />
           </div>
           <div>
-            <img className="w-60 mx-10" src={photo2} alt="" />
-            <h1 className="text-xl text-white mt-[-3rem]  mx-14">
+            <img className="w-60 mx-1 md:mx-10" src={photo2} alt="" />
+            <h1 className="text-xl hidden md:block text-white mt-[-3rem]  mx-14">
               <Typewriter
                 options={{
                   autoStart: true,
@@ -54,8 +56,8 @@ const PersonalBranding = () => {
             </h1>
           </div>
           <div>
-            <img className="w-56 branding-img" src={photo3} alt="" />
-            <img className="w-56 branding-img" src={photo4} alt="" />
+            <img className="w-52 md:56 branding-img" src={photo3} alt="" />
+            <img className="w-52 md:w-56 branding-img" src={photo4} alt="" />
           </div>
         </div>
       </div>
