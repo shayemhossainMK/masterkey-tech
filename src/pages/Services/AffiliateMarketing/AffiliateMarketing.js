60b1518d95287927a0bfdffe23a7ef519e2a0a4a
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-15 12:30:30 +0600

    affiliate marketing added

--- src/pages/Services/AffiliateMarketing/AffiliateMarketing.js
+++ src/pages/Services/AffiliateMarketing/AffiliateMarketing.js
@@ -15,18 +15,20 @@ import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 const AffiliateMarketing = () => {
   return (
     <div>
-      <SubNavbar></SubNavbar>
+      <div className="hidden md:block md:sticky  md:top-20 md:z-50">
+        <SubNavbar></SubNavbar>
+      </div>
       <div className="bg-primary">
         <div className="text-center">
-          <h1 className="uppercase text-white text-2xl pt-5">
+          <h1 className="uppercase text-white text-2xl pt-10 md:pt-5">
             AFFILIATE MARKETING SOLUTIONS
           </h1>
         </div>
-        <div className="px-5 md:px-16 pt-3 md:pt-10">
-          <div className="flex justify-center pb-14">
-            <div className="px-5 md:px-30">
-              <img className="w-[550px]" src={asset3} alt="" />
-              <p className="px-5 md:px-10 text-white text-xl mt-[-15rem] ">
+        <div className="px-5 md:px-16 pt-20 md:pt-10">
+          <div className="md:flex justify-center pb-14">
+            <div className="px-1 md:px-30">
+              <img className="w-full md:w-[550px]" src={asset3} alt="" />
+              <p className="px-5 md:px-10 mb-20 md:mb-0 text-white text-xs md:text-xl mt-[-10rem] md:mt-[-15rem] ">
                 For any e-commerce business Affiliate <br /> Marketing is a
                 must. Because it contributes <br /> more than 20% of daily
                 traffic and 10% of the <br /> sale. In Masterkey we have the
@@ -49,14 +51,14 @@ const AffiliateMarketing = () => {
       </div>
       <div>
         <div className="">
-          <div className="flex ">
-            <div className="h-full sticky top-0 z-10  bg-gradient-to-tl from-[#6A0DAD] to-gray-800">
+          <div className="md:flex ">
+            <div className="h-full md:sticky top-0 z-10  bg-gradient-to-tl from-[#6A0DAD] to-gray-800">
               <img
                 className="w-full h-[650px] mix-blend-overlay relative z-10 opacity-70"
                 src={photo3}
                 alt=""
               />
-              <div className=" absolute top-56 z-20 px-5">
+              <div className="absolute top-[65rem] md:top-56 z-20 px-5">
                 <h1 className="text-white text-4xl">
                   AFFILIATE <br /> MARKETING <br /> SOLUTIONS
                 </h1>
@@ -68,34 +70,34 @@ const AffiliateMarketing = () => {
                 ></progress>
               </div>
             </div>
-            <div className="ml-[-4rem] z-20 py-10">
-              <div className="flex">
+            <div className="pl-2 md:ml-[-4rem] z-20 py-10">
+              <div className="flex items-center md:items-stretch">
                 <div className="px-7 py-7 bg-base-100 rounded-lg shadow-2xl">
                   <img className="w-16 mx-auto" src={asset60} alt="" />
                 </div>
-                <div className="px-5 justify-between flex items-center mx-5 bg-base-100 rounded-lg shadow-xl w-full">
+                <div className="px-5 justify-between flex items-center mx-5 bg-base-100 rounded-lg shadow-xl w-full py-1 md:py-0">
                   <p>Making a proper strategic affiliate business plan.</p>
                   <p className="text-4xl  text-gray-400">01</p>
                 </div>
               </div>
-              <div className="flex mt-5">
+              <div className="flex items-center md:items-stretch mt-5">
                 <div className="px-7 py-6 bg-base-100 rounded-lg shadow-2xl">
                   <img className="w-16 mx-auto" src={asset59} alt="" />
                 </div>
-                <div className="px-5 flex items-center mx-5 bg-base-100 rounded-lg shadow-lg w-full justify-between">
+                <div className="px-5 flex items-center mx-5 bg-base-100 rounded-lg shadow-lg w-full justify-between py-1 md:py-0">
                   <p className="">
                     Choosing or creating and setting up the right Affiliate
                     tracking software.
                   </p>
-                  <p className="text-4xl text-gray-400">02</p>
+                  <p className="text-4xl text-gray-400 pl-2 md:pl-0">02</p>
                 </div>
               </div>
-              <div className="flex  mt-5">
+              <div className="flex items-center md:items-stretch mt-5">
                 <div className="px-8 py-7 bg-base-100 rounded-lg shadow-2xl">
                   <img className="w-14 mx-auto" src={asset57} alt="" />
                 </div>
-                <div className="px-7 flex items-center justify-between w-full mx-5 bg-base-100 rounded-lg shadow-lg">
-                  <p className="pr-2">
+                <div className="px-7 flex items-center justify-between w-full mx-5 bg-base-100 rounded-lg shadow-lg py-1 md:py-0">
+                  <p className="">
                     Set up email automation, trigger emails, and pixels, add
                     resources for affiliates, and then test the platform and
                     make it ready to launch
@@ -103,22 +105,22 @@ const AffiliateMarketing = () => {
                   <p className="text-4xl text-gray-400">03</p>
                 </div>
               </div>
-              <div className="flex mt-5">
+              <div className="flex items-center md:items-stretch mt-5">
                 <div className="px-9 py-6 bg-base-100 rounded-lg shadow-2xl">
                   <img className="w-12 mx-auto mt-3" src={asset56} alt="" />
                 </div>
-                <div className="px-5 flex items-center mx-5 bg-base-100 rounded-lg shadow-lg w-full justify-between">
+                <div className="px-5 flex items-center mx-5 bg-base-100 rounded-lg shadow-lg w-full justify-between py-1 md:py-0">
                   <p className="">
                     Recruiting active, engaging affiliates that can drive sales.
                   </p>
                   <p className="text-4xl text-gray-400">04</p>
                 </div>
               </div>
-              <div className="flex  mt-5">
+              <div className="flex items-center md:items-stretch mt-5">
                 <div className="px-7 py-3 bg-base-100 rounded-lg shadow-2xl">
                   <img className="w-16 mt-4 mx-auto" src={asset53} alt="" />
                 </div>
-                <div className="px-5 flex items-center mx-5 bg-base-100 rounded-lg shadow-lg justify-between w-full">
+                <div className="px-5 flex items-center mx-5 bg-base-100 rounded-lg shadow-lg justify-between w-full py-1 md:py-0">
                   <p className="">
                     Managing affiliates to grow the affiliate campaign.
                   </p>
