1ce92d2d5037d19456f34fe9647f41566828dbd5
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-15 13:41:29 +0600

    new code added

--- src/pages/Services/BulkSms/BulkSms.js
+++ src/pages/Services/BulkSms/BulkSms.js
@@ -9,27 +9,31 @@ import photo4 from "../../../images/serviceImg/Asset 77.svg";
 const BulkSms = () => {
   return (
     <div>
-      <SubNavbar></SubNavbar>
+      <div className="hidden md:block md:sticky  md:top-20 md:z-50">
+        <SubNavbar></SubNavbar>
+      </div>
       <div className="bulk-container h-full pb-28">
-        <div className="flex justify-center items-center h-screen">
+        <div className="md:flex justify-center items-center h-screen">
           <div>
-            <div className="px-5">
-              <h1 className="text-white text-4xl fixed mt-[-7rem]">
+            <div className="px-5 py-10">
+              <h1 className="text-white text-4xl md:fixed md:mt-[-7rem]">
                 BULK SMS <br /> SERVICE
               </h1>
-              <h4 className="text-xl  text-white uppercase fixed">Service</h4>
+              <h4 className="text-xl  text-white uppercase mt-6 md:mt-0 md:fixed">
+                Service
+              </h4>
               <progress
-                class="progress progress-warning w-56 fixed mt-10 "
+                class="progress progress-warning w-56 md:fixed mt-3 md:mt-10 "
                 value="100"
                 max="100"
               ></progress>
             </div>
           </div>
           <div
-            className="px-5 flex
+            className="px-5 md:flex
           flex-col items-center  md:px-36 mt-5"
           >
-            <div className=" ml-96">
+            <div className=" md:ml-96">
               <p className="p-5 rounded-xl bg-base-100 mb-6">
                 We provide a comprehensive messaging platform that has
                 integration with bulk SMS, WhatsApp, and email. We have Sorted
@@ -41,23 +45,23 @@ const BulkSms = () => {
                 initiating a selling funnel.
               </p>
             </div>
-            <div className="mt-14 grid grid-cols-3 gap-10 ml-96">
-              <div className="p-5 rounded-lg bg-base-100 w-36 h-28">
+            <div className="mt-14 grid grid-cols-3 gap-4 md:gap-10 md:ml-96">
+              <div className="p-5 rounded-lg bg-base-100 md:w-36 h-28">
                 <img className="w-12 h-10 mx-auto" src={photo1} alt="" />
                 <p className="text-center mt-3 text-xs">WhatsApp Messaging</p>
               </div>
-              <div className="p-5 rounded-lg bg-base-100 w-36">
+              <div className="p-5 rounded-lg bg-base-100 md:w-36">
                 <img className="w-14 mx-auto" src={photo2} alt="" />
                 <p className="text-center text-xs mt-3">
                   Masterkey User Database & SMS Platform
                 </p>
               </div>
-              <div className="p-5 rounded-lg bg-base-100 w-36 h-28">
+              <div className="p-5 rounded-lg bg-base-100 md:w-36 h-28">
                 <img className="w-12 mx-auto" src={photo3} alt="" />
                 <p className="text-center text-xs mt-3">Email Services</p>
               </div>
               <div></div>
-              <div className="p-5 rounded-lg bg-base-100 w-36">
+              <div className="p-5 rounded-lg bg-base-100 md:w-36">
                 <img className="w-12 mx-auto" src={photo4} alt="" />
                 <p className="text-center text-xs mt-3">Email Services</p>
               </div>
